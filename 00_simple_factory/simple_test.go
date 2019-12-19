8ac8a47f277cf85059871575a0fe8082a1a56861
wuqi <wuqi226@gmail.com>
2019-12-19 11:23:03 +0800

    push log

--- 00_simple_factory/simple_test.go
+++ 00_simple_factory/simple_test.go
@@ -1,11 +1,15 @@
 package simplefactory
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 //TestType1 test get hiapi with factory
 func TestType1(t *testing.T) {
 	api := NewAPI(1)
 	s := api.Say("Tom")
+	log.Printf("test type1 : %v\n", s)
 	if s != "Hi, Tom" {
 		t.Fatal("Type1 test fail")
 	}
@@ -14,6 +18,7 @@ func TestType1(t *testing.T) {
 func TestType2(t *testing.T) {
 	api := NewAPI(2)
 	s := api.Say("Tom")
+	log.Printf("test type2 : %v\n", s)
 	if s != "Hello, Tom" {
 		t.Fatal("Type2 test fail")
 	}
